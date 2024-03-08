73508e802ca8f6d46874a8492162b86df9f99c87
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-03-08 19:25:02 +0100

    update

--- src/app/api/axios/api.ts
+++ src/app/api/axios/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: `${process.env.BACK_END_URL}`,
 });
 
 export const localApi = axios.create({
