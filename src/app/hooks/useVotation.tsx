a2c0e235aad92203773c25b648a8976104eb3f3b
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-03-09 23:51:14 +0100

    Update useVotation.tsx

--- src/app/hooks/useVotation.tsx
+++ src/app/hooks/useVotation.tsx
@@ -1,78 +1,7 @@
-// "use client";
-// import { createContext, useState, ReactNode, useContext, useEffect } from "react";
-// import { ContryData } from "../@types/Types";
-// import { api, localApi } from '../api/axios/api';
-// import { ErrorMessage } from "../utils/FeedBack/Messages";
-
-// interface VotationContextData {
-//   agree: ArrayID[],
-//   disagree: ArrayID[],
-//   abst: ArrayID[],
-//   members: ContryData[],
-//   SelectChange: (index: number, newPosition: ContryData['state']) => void,
-// }
-
-// const VotationContext = createContext(
-//   {} as VotationContextData);
-
-// interface VotationProviderProps {
-//   children: ReactNode,
-// }
-
-// type ArrayID = {
-//   id: number,
-// }
-
-// export function VotationContextProvider({ children }: VotationProviderProps) {
-//   const [members, setMembers] = useState<ContryData[] | any[]>([]);
-//   const [agree, setAgree] = useState<ArrayID[]>([]);
-//   const [disagree, setDisagree] = useState<ArrayID[]>([]);
-//   const [abst, setAbts] = useState<ArrayID[]>([]);
-
-//   useEffect(() => {
-//     async function getMembers() {
-//       const response = await localApi.get("/api/members/view");
-//       const members = response.data;
-//       setMembers(members);
-//     }
-//     getMembers();
-//     console.log({ agree, disagree, abst });
-//   }, [abst, agree, disagree]);
-
-//   async function SelectChange(index: number, newState: ContryData['state']) {
-//     const updatedContries: ContryData[] = [...members];
-//     updatedContries[index] = { ...updatedContries[index], state: newState };
-
-//     if (updatedContries[index].state === "Concorda") {
-//       const newId = updatedContries[index].id;
-//       setAgree((prev) => [...prev, { id: newId}] );
-//     }
-
-//     if (updatedContries[index].state === "Discorda") {
-//       const newId = updatedContries[index].id;
-//       setDisagree((prev) => [...prev, { id: newId}]);
-//     }
-
-//     if (updatedContries[index].state === "Em abstenção") {
-//       const newId = updatedContries[index].id;
-//       setAbts((prev) => [...prev, { id: newId}]);
-//     }
-//   }
-  
-//   return (
-//     <VotationContext.Provider value={{ SelectChange, members, abst, agree, disagree }}>
-//       {children}
-//     </VotationContext.Provider>
-//   )
-// }
-
-// export function UseVotation() {
-//   return useContext(VotationContext);
-// }
 "use client";
 import { createContext, useState, ReactNode, useContext, useEffect } from "react";
 import { ContryData } from "../@types/Types";
-import { api } from '../api/axios/api';
+import axios from "axios";
 
 interface VotationContextData {
   agree: ArrayID[],
@@ -101,7 +30,7 @@ export function VotationContextProvider({ children }: VotationProviderProps) {
 
   useEffect(() => {
     async function getMembers() {
-      const response = await api.get("/members");
+      const response = await axios.get("https://api-rg.onrender.com/members");
       const members = response.data;
       setMembers(members);
     }
