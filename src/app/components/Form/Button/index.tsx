d5abcddaa5bff007a1c1c1ab2fff7011f09948e5
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-03-09 11:20:39 +0100

    Update index.tsx

--- src/app/components/Form/Button/index.tsx
+++ src/app/components/Form/Button/index.tsx
@@ -10,7 +10,7 @@ type ButtonProps = ComponentProps<"button"> & {
 export function Button({ children, className, isSubmitting, ...restProps }: ButtonProps) {
   return (
     <button 
-      className={twM(`w-full rounded-md py-2 mt-4 bg-main-500 ${!isSubmitting && "hover:bg-blue-700"} transition-colors text-white font-medium`, className)}
+      className={twM(`w-full rounded-md flex items-center justify-center py-2 mt-4 bg-main-500 ${!isSubmitting && "hover:bg-blue-700"} transition-colors text-white font-medium`, className)}
       {...restProps}
     > 
       {children}
