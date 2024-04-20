f76d36e3b05168eea07d5761cc5dd99d6cb6d17c
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-04-20 19:33:18 +0100

    Update page.tsx

--- src/app/(pages)/view_relatories/page.tsx
+++ src/app/(pages)/view_relatories/page.tsx
@@ -4,22 +4,7 @@ import { RelatoryList } from "./RelatoryList";
 export default function ViewRelatories() {
   return (
     <PageRoot classeName="w-full">
-      <div className="w-full h-full flex flex-col gap-4">
-        <div className="w-full flex justify-between items-center text-gray-500 px-10">
-          <h1 className="font-semibold">
-            Ficheiro
-          </h1>
-          <h1 className="font-semibold">
-            Data
-          </h1>
-          <h1 className="font-semibold">
-            Acção
-          </h1>
-        </div>
-        <div className="w-full h-auto flex flex-col gap-2 py-2 px-4">
-          <RelatoryList />
-        </div>
-      </div>
+      <RelatoryList />
     </PageRoot>
   );
 }
